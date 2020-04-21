2383783e01b350c5870a96159715b13ed13d27b8
nurdin73 <nurdin.reverse73@gmail.com>
2020-04-21 18:06:42 +0700

    layouting table

--- src/Pages/Admin/Archive/Archive_users/Buyer_archive/index.js
+++ src/Pages/Admin/Archive/Archive_users/Buyer_archive/index.js
@@ -1,11 +1,49 @@
 import React, { Component } from 'react';
+import { Card, CardBody } from 'reactstrap'
+import Datatable from '../../../../../Components/Datatable';
+const dataSet = [
+  [ 
+    "1", 
+    "Cepot", 
+    "example@gmail.com", 
+    'Rempah rempah', 
+    "Cirebon",
+    `<div class="btn-group" role="group" aria-label="Basic example">
+        <button class="btn btn-sm btn-success"><i class="fa fa-fw fa-refresh"></i> Restore</button>
+        <button class="btn btn-sm btn-danger"><i class="fa fa-fw fa-trash"></i> Delete</button> 
+      </div>`
+  ],
+  [ 
+    "2", 
+    "Cepot", 
+    "example@gmail.com", 
+    'Rempah rempah', 
+    "Cirebon",
+    `<div class="btn-group" role="group" aria-label="Basic example">
+        <button class="btn btn-sm btn-success"><i class="fa fa-fw fa-refresh"></i> Restore</button>
+        <button class="btn btn-sm btn-danger"><i class="fa fa-fw fa-trash"></i> Delete</button> 
+      </div>`
+  ],
+];
 
+const columns = [
+  {title:"No"},
+  {title:"Name Buyer"},
+  {title:"Email"},
+  {title:"Phone Number"},
+  {title:"Addess"},
+  {title:"Action"},
+]
 class Buyer_archive extends Component {
   render() {
     return (
       <div className="animated fadeIn">
         {/* Content in here */}
-        <h1>ARCHIVE USERS BUYER DISINI</h1>
+        <Card>
+          <CardBody>
+            <Datatable data={dataSet} column={columns} orderable={false} searchable={false} target={[0,5]}/>
+          </CardBody>
+        </Card>
       </div>
     );
   }
--- src/Pages/Admin/Archive/Archive_users/Seller_archive/index.js
+++ src/Pages/Admin/Archive/Archive_users/Seller_archive/index.js
@@ -1,11 +1,49 @@
 import React, { Component } from 'react';
+import { Card, CardBody } from 'reactstrap'
+import Datatable from '../../../../../Components/Datatable';
+const dataSet = [
+  [ 
+    "1", 
+    "Jamal", 
+    "example@gmail.com", 
+    'Cirebon', 
+    "Bung bung",
+    `<div class="btn-group" role="group" aria-label="Basic example">
+        <button class="btn btn-sm btn-success"><i class="fa fa-fw fa-refresh"></i> Restore</button>
+        <button class="btn btn-sm btn-danger"><i class="fa fa-fw fa-trash"></i> Delete</button> 
+      </div>`
+  ],
+  [ 
+    "2", 
+    "Jamal", 
+    "example@gmail.com", 
+    'Cirebon', 
+    "Bung bung",
+    `<div class="btn-group" role="group" aria-label="Basic example">
+        <button class="btn btn-sm btn-success"><i class="fa fa-fw fa-refresh"></i> Restore</button>
+        <button class="btn btn-sm btn-danger"><i class="fa fa-fw fa-trash"></i> Delete</button> 
+      </div>`
+  ],
+];
 
+const columns = [
+  {title:"No"},
+  {title:"Name Seller"},
+  {title:"Email"},
+  {title:"Phone Number"},
+  {title:"Address"},
+  {title:"Action"},
+]
 class Seller_archive extends Component {
   render() {
     return (
       <div className="animated fadeIn">
         {/* Content in here */}
-        <h1>ARCHIVE USER SELLER DISINI</h1>
+        <Card>
+          <CardBody>
+            <Datatable data={dataSet} column={columns} orderable={false} searchable={false} target={[0,5]}/>
+          </CardBody>
+        </Card>
       </div>
     );
   }
